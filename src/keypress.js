d59feb9dbb3535c5d25eebd28f1fbefd9459f2fd
RaphaelMK <tsukizooff@outlook.fr>
2023-01-19 19:56:30 +0100

    Fix Jump bug

---
+++ src/keypress.js
@@ -0,0 +1,24 @@
+
+// vérifier si une touche est pressée 
+function keyPressed() {
+    
+    if (keyCode === ESCAPE) {
+        isInPaused = !isInPaused
+    }
+
+
+    if (keyCode === 32) {
+        spaceKeyIsPressed = true;
+    }
+}
+
+
+
+
+// vérifier si une touche est relâchée 
+function keyReleased() {
+    if (keyCode === 32) {
+        spaceKeyIsPressed = false;
+        characterDoubleJumping = true;
+    }
+}
