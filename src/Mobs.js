7db012d3380186f8413060555f1cd81eecabaa67
Nay <ilan.outyey@gmail.com>
2023-03-13 08:03:26 +0100

    PNJ Collides Bug Fixed

--- src/Mobs.js
+++ src/Mobs.js
@@ -1,171 +1,185 @@
-/* -------------------------------------------------------------------------- */
-/*                    Collisions de l'entité avec le monde                    */
-/* -------------------------------------------------------------------------- */
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                    Collisions de l'entité avec le monde                    */
+//^ /* -------------------------------------------------------------------------- */
+function handleCollisionMobs(
+  agentX,
+  agentY,
+  agentWidth,
+  agentHeight,
+  agentDirection,
+  objectX,
+  objectY,
+  objectWidth,
+  objectHeight,
+  velocityY,
+  jumpCount,
+  isJumping
+) {
 
-function handleCollisionMobs(agentX, agentY, agentWidth, agentHeight, agentDirection, objectX, objectY, objectWidth, objectHeight, velocityY, jumpCount, isJumping) {
+  let haveToJump = false;
 
-    //* Vérifier si les boîtes se chevauchent
-    if (rectIsInRect(agentX, agentY, agentWidth, agentHeight, objectX, objectY, objectWidth, objectHeight)) {
-  
-        //~ Collisions dessus / dessous de l'objet
-        if ((agentX < objectX + objectWidth - (objectWidth / 10)) && (agentX + agentWidth > objectX + (objectWidth / 10))) {
-            //? collisions en dessous de l'objet
-            if (agentY < objectY + objectHeight && agentY > objectY) {
-                agentY = objectY + objectHeight
-  
-                velocityY = 0;
+  if (
+    rectIsInRect(
+      agentX,
+      agentY,
+      agentWidth,
+      agentHeight,
+      objectX,
+      objectY,
+      objectWidth,
+      objectHeight
+    )
+  ) {
 
-            }
-            //? collision au dessus de l'objet
-            else if (agentY + agentHeight > objectY && agentY < objectY) {
-  
-                agentY = objectY - agentHeight
 
-                jumpCount = 0;
-                isJumping = false
-                if (!isJumping){
-                    velocityY = 0
-                }
+    //& Collisions dessus / dessous de l'objet
+    if (
+      agentX < objectX + objectWidth - objectWidth / 10 &&
+      agentX + agentWidth > objectX + objectWidth / 10
+    ) {
+      //? collisions en dessous de l'objet
+      if (agentY < objectY + objectHeight && agentY > objectY) {
+        agentY = objectY + objectHeight;
 
-            }   
-            
-            return [agentX, agentY, velocityY, jumpCount, isJumping, haveToJump]
+        velocityY = 0;
+      }
+      //? collision au dessus de l'objet
+      else if (agentY + agentHeight > objectY && agentY < objectY) {
+        agentY = objectY - agentHeight;
+
+        jumpCount = 0;
+        isJumping = false;
+        if (!isJumping) {
+          velocityY = 0;
         }
+      }
+    }
 
-  
-        //~ Collisions gauche / droite de l'objet
-        //? si le bas du perso est en dessous du haut du cube + 1/10 de sa hauteur
-        if ((agentY < objectY + objectHeight - (objectHeight / 10)) && (agentY + agentHeight > objectY + (objectHeight / 10))) {
-    
-            //? collisions à droite de l'objet
-            if (agentX + agentWidth > objectX && agentX > objectX) {
-                agentX = objectX + objectWidth
 
-                if (agentDirection == "left"){
 
-                    haveToJump = true
-                }else{
-                    haveToJump = false
-                }
-    
-            //? collisions à gauche de l'objet
-            } else if (agentX < objectX + objectWidth && agentX < objectX) {
-                agentX = objectX - agentWidth
+    //& Collisions sur les côtés de l'objet
+    if (
+      agentY < objectY + objectHeight - objectHeight / 10 &&
+      agentY + agentHeight > objectY + objectHeight / 10
+    ) {
+      //? collisions à droite de l'objet
+      if (agentX + agentWidth > objectX && agentX > objectX) {
+        agentX = objectX + objectWidth;
 
-                if (agentDirection == "right"){
-                    haveToJump = true
-                }else{
-                    haveToJump = false
-                }
+        //? collisions à gauche de l'objet
+      } else if (agentX < objectX + objectWidth && agentX < objectX) {
+        agentX = objectX - agentWidth;
+      }
 
-            }
-            
 
-            
-            return [agentX, agentY, velocityY, jumpCount, isJumping, haveToJump]
-        }  
+      haveToJump = true;
 
     }
-    return [agentX, agentY, velocityY, jumpCount, isJumping, haveToJump]
+
+
+  }
+  // console.log(haveToJump)
+
+
+  return [agentX, agentY, velocityY, jumpCount, isJumping, haveToJump];
 }
 
 
-/* -------------------------------------------------------------------------- */
-/*                              Suivre le joueur                              */
-/* -------------------------------------------------------------------------- */
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                              Suivre le joueur                              */
+//^ /* -------------------------------------------------------------------------- */
+function followPlayer(Mobs) {
 
-function followPlayer(Mobs){
+  //& Initialisation des variables
 
-    Mobs.isFollowing = true
+  Mobs.isFollowing = true;
 
-    let distance = characterPositionX - Mobs.x;
-    let followSpeed = Mobs.followSpeed
-    let MobsHaveToJump = Mobs.haveToJump
+  let distance = characterPositionX - Mobs.x;
+  let followSpeed = Mobs.followSpeed;
+  let MobsHaveToJump = Mobs.haveToJump;
 
-    Mobs.movement = "walk"
+  Mobs.movement = "walk";
+  Mobs.direction = "right";
+
+  //& Se diriger vers le perso
+  if (distance < 0) {
+    Mobs.direction = "left";
+    followSpeed *= -1;
+  }
+
+  //& Si je dois sauter, ... . Sinon, marcher.
+  if (MobsHaveToJump) {} else {
+    Mobs.stepCount += followSpeed;
+  }
+}
+
+
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                     Un mob se ballade / fait une ronde                     */
+//^ /* -------------------------------------------------------------------------- */
+function doRound(Mobs) {
+
+
+
+  //& Initialisation des variables
+
+  Mobs.movement = "walk";
+  Mobs.isFollowing = false;
+
+  let CurrentX = Mobs.x;
+  let walkAmount = Mobs.stepCount;
+
+  let MobEnd = Mobs.xEnd;
+  let MobStart = Mobs.xStart;
+
+  let haveToJump = Mobs.haveToJump;
+  // console.log(haveToJump);
+
+  //& FAIRE UNE RONDE
+  if (CurrentX >= MobEnd) {
+    Mobs.direction = "left";
+  } else if (CurrentX <= MobStart) {
     Mobs.direction = "right";
+  }
 
-    //? se diriger vers le perso 
-    if (distance < 0){
-        Mobs.direction = "left";
-        followSpeed *= -1
+
+  //& Se déplacer
+  //* Si je vais à droite
+  if (Mobs.direction == "right") {
+
+    //* Si je dois sauter, s'orienter dans l'autre sens
+    if (haveToJump) {
+      Mobs.direction = "left";
+      haveToJump = false;
+      walkAmount -= Mobs.speed
+
+
+
+      //* Sinon, marcher
+    } else {
+      walkAmount += Mobs.speed;
     }
 
-    if (!MobsHaveToJump){
-        Mobs.stepCount += followSpeed;
-    }else{
-        Mobs.haveToJump = false
+
+    //* Si je vais à gauche
+  } else {
+
+    //* Si je dois sauter, s'orienter dans l'autre sens
+    if (haveToJump) {
+      Mobs.direction = "right";
+      haveToJump = false;
+      walkAmount += Mobs.speed
+
+
+      //* Sinon, marcher
+    } else {
+      walkAmount -= Mobs.speed;
     }
+  }
 
-
-}
-
-
-/* -------------------------------------------------------------------------- */
-/*                     Un mob se ballade / fait une ronde                     */
-/* -------------------------------------------------------------------------- */
-function doRound(Mobs){
-    Mobs.movement = "walk"
-
-    Mobs.isFollowing = false
-
-    let CurrentX = Mobs.x
-    let walkAmount = Mobs.stepCount
-
-    let MobEnd = Mobs.xEnd
-    let MobStart = Mobs.xStart
-
-    let MobsHaveToJump = Mobs.haveToJump
-
-
-
-    //& FAIRE UNE RONDE
-    if (CurrentX >= MobEnd){
-        Mobs.direction = "left"
-    }else if (CurrentX <= MobStart){
-        Mobs.direction = "right"
-    }
-    
-    if (Mobs.color == "blue"){
-        rect(MobStart, 200, 50, 50)
-        rect(MobEnd, 200, 60, 60)
-
-    }
-
-    
-    //& Se déplacer
-    if (Mobs.direction == "right"){
-        
-        if (haveToJump){
-            Mobs.direction = "left"
-            walkAmount -= Mobs.speed
-            haveToJump = false
-            
-        }else{
-            
-            walkAmount += Mobs.speed
-            
-        }
-    }else{
-        if (haveToJump){
-            Mobs.direction = "right"
-
-          
-            walkAmount += Mobs.speed
-
-            haveToJump = false
-            
-
-        }else{
-            walkAmount -= Mobs.speed
-            
-        }
-        
-    }
-
-     
-    Mobs.haveToJump = MobsHaveToJump
-    Mobs.stepCount = walkAmount
-    Mobs.xStart = MobStart
-    Mobs.xEnd = MobEnd
+  //& Retourner les variables
+  Mobs.haveToJump = haveToJump;
+  Mobs.stepCount = walkAmount;
+  Mobs.xStart = MobStart;
+  Mobs.xEnd = MobEnd;
 }
--- src/PNJ.js
+++ src/PNJ.js
@@ -1,264 +1,367 @@
-//#region MANAGER 
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                                 PNJ MANAGER                                */
+//^ /* -------------------------------------------------------------------------- */
 function PNJManager() {
-
-    // ~ Draw des PNJ en EXTERIEUR
-    if (engineOne) {
-        PNJ(ForPNJ.PNJS.PNJ1);
-        PNJ(ForPNJ.PNJS.PNJ2);
-    }
-    else {
-        drawPNJInside(ForPNJ.pnj.PNJ3);
-    }
+  // ~ Draw des PNJ en EXTERIEUR
+  if (engineOne) {
+    // PNJ(ForPNJ.PNJS.PNJ1);
+    PNJ(ForPNJ.PNJS.PNJ2);
+  } else {
+    drawPNJInside(ForPNJ.PNJS.PNJ1);
+  }
 }
-//#endregion
-
-
-//#region FONCTION POUR PNJ
 
 
 
-// DRAW PNJ
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                              DRAW PNJ INSIDE                               */
+//^ /* -------------------------------------------------------------------------- */
 let drawPNJInside = (pnj) => {
+  // ~ Variables positions PNJ
+  let PNJDistance = pnj.distance + pnj.x;
+  let PNJY = pnj.y + yStartHouse;
+  let PNJEnd = PNJDistance + xStartHouse;
+  let PNJStart = pnj.x + xStartHouse;
+  let CurrentX = pnj.x + pnj.NbrePas + xStartHouse;
 
-    // ~ Variables positions PNJ
-    let PNJDistance = pnj.distance + pnj.x
-    let PNJY = pnj.y + yStartHouse
-    let PNJEnd = PNJDistance + xStartHouse;
-    let PNJStart = pnj.x + xStartHouse
-    let CurrentX = pnj.x + pnj.NbrePas + xStartHouse;
+  // ~ Variables Collisions / HitBox PNJ
+  let VillagerBoundingBox = expandRect(
+    CurrentX,
+    PNJY,
+    pnj.width,
+    pnj.height,
+    2,
+    2
+  );
+  let seePlayer = rectIsInRect(
+    characterInsidePosX,
+    characterInsidePosY,
+    characterBoundingBoxWidth,
+    characterBoundingBoxHeight,
+    VillagerBoundingBox[0],
+    VillagerBoundingBox[1],
+    VillagerBoundingBox[2],
+    VillagerBoundingBox[3]
+  );
 
-    // ~ Variables Collisions / HitBox PNJ
-    let VillagerBoundingBox = expandRect(CurrentX, PNJY, pnj.width, pnj.height, 2, 2)
-    let seePlayer = rectIsInRect(characterInsidePosX, characterInsidePosY, characterBoundingBoxWidth, characterBoundingBoxHeight, VillagerBoundingBox[0], VillagerBoundingBox[1], VillagerBoundingBox[2], VillagerBoundingBox[3])
-
-    // ~ Debug Mod
-    if (debugMod) {
-        fill(255, 0, 0, 70)
-        rect(VillagerBoundingBox[0], VillagerBoundingBox[1], VillagerBoundingBox[2], VillagerBoundingBox[3])
-        fill(255)
+  // ~ Debug Mod
+  if (debugMod) {
+    fill(255, 0, 0, 70);
+    rect(
+      VillagerBoundingBox[0],
+      VillagerBoundingBox[1],
+      VillagerBoundingBox[2],
+      VillagerBoundingBox[3]
+    );
+    fill(255);
+  }
+  // ~ Direction left
+  if (CurrentX > PNJEnd) {
+    pnj.direction = "left";
+  }
+  // ~ Direction right
+  if (CurrentX < PNJStart) {
+    pnj.direction = "right";
+  }
+  // ~ Hitbox / Collisions
+  if (seePlayer === false) {
+    pnj.movement = "walk";
+    
+    if (pnj.direction === "right") {
+      pnj.NbrePas += pnj.vitesse;
     }
-    // ~ Direction left
-    if (CurrentX > PNJEnd) {
-        pnj.direction = "left";
-    };
-    // ~ Direction right
-    if (CurrentX < PNJStart) {
-        pnj.direction = "right";
-    };
-    // ~ Hitbox / Collisions
-    if (seePlayer === false) {
-        pnj.movement = "walk"
-
-        if (pnj.direction === "right") {
-            pnj.NbrePas += pnj.vitesse;
-        };
-        if (pnj.direction === "left") {
-            pnj.NbrePas -= pnj.vitesse;
-        };
-
+    if (pnj.direction === "left") {
+      pnj.NbrePas -= pnj.vitesse;
+    }
+  } else {
+    pnj.movement = "idle";
+    if (characterInsidePosX >= CurrentX) {
+      pnj.direction = "right";
     } else {
-        pnj.movement = "idle"
-        if (characterInsidePosX >= CurrentX) {
-            pnj.direction = "right"
-        } else {
-            pnj.direction = "left"
-        }
+      pnj.direction = "left";
     }
-    //~ Creation du PNJ
-    animationPNJ(pnj, CurrentX, PNJY, pnj.width, pnj.height, pnj.direction, pnj.movement)
-}
+  }
+  //~ Creation du PNJ
+  animationPNJ(
+    pnj,
+    CurrentX,
+    PNJY,
+    pnj.width,
+    pnj.height,
+    pnj.direction,
+    pnj.movement,
+    pnj.color,
+  );
+};
 
 
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                                    DRAW PNJ                                */
+//^ /* -------------------------------------------------------------------------- */
+function PNJ(pnj) {
 
-function PNJ (pnj){
-    let PNJStart = pnj.globalStartX + xStartWorld
-    let PNJDistance = pnj.distance + PNJStart
-    let PNJEnd = PNJDistance;
+  //& Initialisation des variables
 
-    pnj.x = pnj.globalStartX + xStartWorld + pnj.stepCount;
-    pnj.y = pnj.y + yStartWorld
+  let PNJStart = pnj.globalStartX + xStartWorld;
+  let PNJDistance = pnj.distance + PNJStart;
+  let PNJEnd = PNJDistance;
+
+  pnj.x = pnj.globalStartX + xStartWorld + pnj.stepCount;
+  pnj.y = pnj.y + yStartWorld;
+
+  let PNJX = pnj.x;
+  let PNJY = pnj.y;
+
+  let PNJHaveToJump = pnj.haveToJump;
+
+  let PNJWidth = pnj.width;
+  let PNJHeight = pnj.height;
+
+  let PNJVelocityY = pnj.velocityY;
+  let PNJMass = pnj.mass;
+
+  let PNJIsJumping = pnj.isJumping;
+  let PNJJumpCount = pnj.jumpCount;
+
+  //& Ajout de la gravité au PNJ
+  let gravityReturns = getPositionWithGravity(
+    PNJY,
+    PNJVelocityY,
+    gravityForce,
+    PNJMass
+  );
+  PNJY = gravityReturns[0];
+  PNJVelocityY = gravityReturns[1];
+
+  let mapsToCheck = getMapsToCheck(characterPositionX, characterPositionY);
+
+  let collide = false;
+    
+  //& Ajoute les collisions pour toute les maps autour du perso
+  for (let i = 0; i < mapsToCheck.length; i++) {
+    let currentMapToCheck = mapsToCheck[i];
+    let currentMapToCheckName =
+      World.worldsMap[currentMapToCheck[1]][currentMapToCheck[0]];
+
+    //~ Récupère la couche des collisions sur la map
+    let currentMapTableColliders = Maps[currentMapToCheckName].layers[1];
+
+    
+    //~ Pour chaque carré dans le tableau
+    for (let row = 0; row < currentMapTableColliders.length; row++) {
+      for (
+        let column = 0; column < currentMapTableColliders[row].length; column++
+      ) {
+        //~ Lui donner une collision
+        let thisObject = currentMapTableColliders[row][column];
+
+        let thisObjectX =
+          rectWidth * Maps.numberOfRow * currentMapToCheck[0] +
+          (xStartWorld + rectWidth * column);
+        let thisObjectY =
+          rectHeight * Maps.numberOfColumns * currentMapToCheck[1] +
+          (yStartWorld + rectHeight * row);
 
 
-    let PNJX = pnj.x
-    let PNJY = pnj.y
+        //& Collisions
+        if (thisObject > 0) {
+          [
+            PNJX,
+            PNJY,
+            PNJVelocityY,
+            PNJJumpCount,
+            PNJIsJumping,
+            PNJHaveToJump,
+          ] = handleCollisionMobs(
+            PNJX,
+            PNJY,
+            PNJWidth,
+            PNJHeight,
+            pnj.direction,
+            thisObjectX,
+            thisObjectY,
+            rectWidth,
+            rectHeight,
+            PNJVelocityY,
+            PNJJumpCount,
+            PNJIsJumping
+          );
 
+          if (PNJHaveToJump){
+            collide = true
+          }
+        }
+        
+      }
+    }
+  }
 
-    let PNJHaveToJump = pnj.haveToJump
-
-
-    let PNJWidth = pnj.width;
-    let PNJHeight = pnj.height;
-
-    let PNJVelocityY = pnj.velocityY;
-    let PNJMass = pnj.mass;
-
-    let PNJIsJumping = pnj.isJumping;
-    let PNJJumpCount = pnj.jumpCount;
-
-
-    let mapsToCheck = getMapsToCheck(characterPositionX, characterPositionY)
-
-    //& Ajout de la gravité au PNJ
-    let gravityReturns = getPositionWithGravity(PNJY,
+  //& Ajouter le saut au PNJ
+  if (collide) {
+    if (!PNJIsJumping && PNJJumpCount < 1) {
+      let jumpReturns = addJump(
+        PNJY,
+        characterJumpHeight,
         PNJVelocityY,
-        gravityForce,
-        PNJMass)
-    PNJY = gravityReturns[0]
-    PNJVelocityY = gravityReturns[1]
+        gravityForce
+      );
 
-
-    //& Ajoute les collisions pour toute les maps autour du perso 
-    for (let i = 0; i < mapsToCheck.length; i++) {
-
-
-        let currentMapToCheck = mapsToCheck[i]
-        let currentMapToCheckName = World.worldsMap[currentMapToCheck[1]][currentMapToCheck[0]]
-
-        //^ Récupère la couche des collisions sur la map
-        let currentMapTableColliders = Maps[currentMapToCheckName].layers[1]
-
-        //^ Pour chaque carré dans le tableau 
-        for (let row = 0; row < currentMapTableColliders.length; row++) {
-            for (let column = 0; column < currentMapTableColliders[row].length; column++) {
-
-                //^ Lui donner une collision
-                let thisObject = currentMapTableColliders[row][column]
-
-
-                let thisObjectX = ((rectWidth * Maps.numberOfRow) * (currentMapToCheck[0])) + (xStartWorld + (rectWidth * column))
-                let thisObjectY = ((rectHeight * Maps.numberOfColumns) * (currentMapToCheck[1])) + (yStartWorld + (rectHeight * row))
-
-
-                if (thisObject > 0) {
-                    [PNJX, PNJY, PNJVelocityY, PNJJumpCount, PNJIsJumping, PNJHaveToJump] = handleCollisionMobs(PNJX, PNJY, PNJWidth, PNJHeight, pnj.direction, thisObjectX, thisObjectY, rectWidth, rectHeight, PNJVelocityY, PNJJumpCount, PNJIsJumping)
-                }
-            }
-        }
+      PNJY = jumpReturns[0];
+      PNJVelocityY = jumpReturns[1];
+      PNJIsJumping = true;
+      PNJJumpCount += 1;
+      pnj.movement = "jump";
+      
+    } else {
+      PNJJumpCount = 0;
     }
+  }
 
-    //& Ajouter le saut au PNJ
-    if (PNJHaveToJump && pnj.isFollowing){
-        
-        if (!PNJIsJumping && PNJJumpCount < 1){
+  //& Retourne les variables
+  pnj.x = PNJX;
+  pnj.y = PNJY;
+  pnj.velocityY = PNJVelocityY;
+  pnj.isJumping = PNJIsJumping;
+  pnj.jumpCount = PNJJumpCount;
 
-            let jumpReturns = addJump(PNJY,
-                characterJumpHeight,
-                PNJVelocityY,
-                gravityForce)
-        
-            PNJY = jumpReturns[0];
-            PNJVelocityY = jumpReturns[1];
-            PNJIsJumping = true
-            PNJJumpCount += 1
-            pnj.movement = "jump"
-        }else{
-            PNJJumpCount = 0
-        }
-    }
+  pnj.xStart = PNJStart;
+  pnj.xEnd = PNJEnd;
+  pnj.haveToJump = collide;
 
-
-    pnj.x = PNJX
-    pnj.y = PNJY
-    pnj.velocityY = PNJVelocityY
-    pnj.isJumping = PNJIsJumping
-    pnj.jumpCount = PNJJumpCount
-
-    pnj.xStart = PNJStart
-    pnj.xEnd = PNJEnd
-    pnj.haveToJump = PNJHaveToJump;
-
-
-    drawPNJ(pnj)
+  PNJMovements(pnj);
 }
 
 
-let drawPNJ = (pnj) => {
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                                PNJ MOVEMENTS                               */
+//^ /* -------------------------------------------------------------------------- */
 
-    // ~ Variables positions PNJ
-    let CurrentX = pnj.x;
-    let PNJY = pnj.y
+let PNJMovements = (pnj) => {
+  //& Variables positions PNJ
+  let CurrentX = pnj.x;
+  let PNJY = pnj.y;
 
-    // ~ Variables Collisions / HitBox PNJ
-    let VillagerBoundingBox = expandRect(CurrentX, PNJY, pnj.width, pnj.height, pnj.detectDistX, pnj.detectDistY)
-    let seePlayer = rectIsInRect(characterPositionX, characterPositionY, characterBoundingBoxWidth, characterBoundingBoxHeight, VillagerBoundingBox[0], VillagerBoundingBox[1], VillagerBoundingBox[2], VillagerBoundingBox[3])
+  //& Variables Collisions / HitBox PNJ
+  let VillagerBoundingBox = expandRect(
+    CurrentX,
+    PNJY,
+    pnj.width,
+    pnj.height,
+    pnj.detectDistX,
+    pnj.detectDistY
+  );
 
-    
-    
 
-    if (!seePlayer){
-        doRound(pnj)
+  //& Zone de détection du PNJ
+  pnj.seePlayer = rectIsInRect(
+    characterPositionX,
+    characterPositionY,
+    characterBoundingBoxWidth,
+    characterBoundingBoxHeight,
+    VillagerBoundingBox[0],
+    VillagerBoundingBox[1],
+    VillagerBoundingBox[2],
+    VillagerBoundingBox[3]
+  );
+
+  //& Si le perso n'est pas vu, faire une ronde
+  if (!pnj.seePlayer) {
+    if (!pnj.haveToJump) {
+      doRound(pnj);
     }
+  }
 
 
-    // ~ Debug Mod
-    if (debugMod) {
-        fill(255, 0, 0, 70)
-        rect(VillagerBoundingBox[0], VillagerBoundingBox[1], VillagerBoundingBox[2], VillagerBoundingBox[3])
-        fill(255)
+
+
+  //& Debug Mod
+  if (debugMod) {
+    fill(0, 0, 255, 70);
+    rect(CurrentX, PNJY, pnj.width, pnj.height);
+    fill(255, 0, 0, 70);
+    rect(
+      VillagerBoundingBox[0],
+      VillagerBoundingBox[1],
+      VillagerBoundingBox[2],
+      VillagerBoundingBox[3]
+    );
+    fill(255);
+  }
+
+  //& Afficher le PNJ
+  animationPNJ(
+    pnj,
+    CurrentX,
+    PNJY,
+    pnj.width,
+    pnj.height,
+    pnj.direction,
+    pnj.movement,
+    pnj.color
+  );
+};
+
+//^ /* -------------------------------------------------------------------------- */
+//^ /*                              // ANIMATION PNJ                              */
+//^ /* -------------------------------------------------------------------------- */
+function animationPNJ(
+  CurrentPNJ,
+  positionX,
+  positionY,
+  width,
+  height,
+  direction,
+  movement,
+  color
+) {
+  fill(color);
+  circle(positionX + 35, positionY - 25, 20);
+
+  let timer = round(millis() / animationSpeed) % 2;
+
+  let PNJTexturesList = [];
+
+  if (movement == "walk") {
+    for (let y = 32; y < 64; y += 32) {
+      for (let x = 0; x < 128; x += 32) {
+        PNJTexturesList.push(PNJTextures.get(x, y, 32, 32));
+      }
     }
+  } else if (movement == "idle") {
+    for (let y = 0; y < 32; y += 32) {
+      for (let x = 0; x < 128; x += 32) {
+        PNJTexturesList.push(PNJTextures.get(x, y, 32, 32));
+      }
+    }
+  } else if (movement == "jump") {
+    for (let y = 64; y < 96; y += 32) {
+      for (let x = 0; x < 128; x += 32) {
+        PNJTexturesList.push(PNJTextures.get(x, y, 32, 32));
+      }
+    }
+  }
+  //? Changer de frame
+  if (timer && !CurrentPNJ.currentFrame) {
+    CurrentPNJ.indexFrame++;
+    CurrentPNJ.currentFrame = true;
+  }
+  if (!timer) {
+    CurrentPNJ.currentFrame = false;
+  }
+  //? Remettre l'index au début
+  if (CurrentPNJ.indexFrame >= PNJTexturesList.length) {
+    CurrentPNJ.indexFrame = 0;
+  }
 
+  let PNJCurrentTextures = PNJTexturesList[CurrentPNJ.indexFrame];
 
-    //~ Creation du PNJ
-    animationPNJ(pnj, CurrentX, PNJY, pnj.width, pnj.height, pnj.direction, pnj.movement, pnj.color)
+  if (direction == "right") {
+    image(PNJCurrentTextures, positionX, positionY, width, height);
+
+    //? direction GAUCHE
+  } else if (direction == "left") {
+    scale(-1, 1);
+    image(PNJCurrentTextures, -positionX - width, positionY, width, height);
+    scale(-1, 1);
+  }
 }
-
-
-
-
-// ANIMATION PNJ
-
-function animationPNJ(CurrentPNJ, positionX, positionY, width, height, direction, movement, color) {
-
-    fill(color)
-    circle(positionX + 35, positionY - 25, 20);
-
-    let timer = (round(millis() / animationSpeed)) % 2
-
-    let PNJTexturesList = []
-
-    if (movement == "walk") {
-
-        for (let y = 32; y < 64; y += 32) {
-            for (let x = 0; x < 192; x += 32) {
-                PNJTexturesList.push(PNJTextures.get(x, y, 32, 32));
-            }
-        }
-    }
-    else if (movement == "idle") {
-
-        for (let y = 0; y < 32; y += 32) {
-            for (let x = 0; x < 192; x += 32) {
-                PNJTexturesList.push(PNJTextures.get(x, y, 32, 32));
-            }
-        }
-    }
-    //? Changer de frame
-    if (timer && !CurrentPNJ.currentFrame) {
-        CurrentPNJ.indexFrame++
-        CurrentPNJ.currentFrame = true
-    }
-    if (!timer) {
-        CurrentPNJ.currentFrame = false
-    }
-    //? Remettre l'index au début 
-    if (CurrentPNJ.indexFrame >= PNJTexturesList.length) {
-        CurrentPNJ.indexFrame = 0
-    }
-
-    let PNJCurrentTextures = PNJTexturesList[CurrentPNJ.indexFrame]
-
-    if (direction == "right") {
-        image(PNJCurrentTextures, positionX, positionY, width, height)
-
-        //? direction GAUCHE
-    } else if (direction == "left") {
-        scale(-1, 1)
-        image(PNJCurrentTextures, -positionX - width, positionY, width, height)
-        scale(-1, 1)
-    }
-}
-
-
-
-
-//#endregion
