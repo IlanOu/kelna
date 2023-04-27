3c8788166bf55dbbfb088249431c3458af1a2548
KiZo <tsukizooff@outlook.fr>
2023-04-27 20:00:31 +0200

    Update keypress

--- src/GUI/keypress.js
+++ src/GUI/keypress.js
@@ -146,12 +146,6 @@ function keyReleased() {
             engineOne = !engineOne;
         }
     }
-
-    //? - du pavé de nombre
-    if (keyCode == 109) {
-        endTheGameCredits = true
-    }
-
 }
 
 
